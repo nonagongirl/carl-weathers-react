777194af4b7ab90e1121c80f2cfb9b599ba0c6e6
nonagongirl <amiin2deep@hotmail.com>
2022-12-17 14:43:45 +0000

    Updated footer links to add GitHub

--- src/Footer.js
+++ src/Footer.js
@@ -6,8 +6,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function Footer() {
   return (
     <div className="Footer">
-      Coded by
-      <a href="http://www.instagram.com/stephaniejay_uk">Stef Braithwaite</a>
+      <a href="https://github.com/nonagongirl/carl-weathers-project">
+        Open-source code{" "}
+      </a>{" "}
+      by{" "}
+      <a href="http://www.instagram.com/stephaniejay_uk"> Stef Braithwaite</a>
     </div>
   );
 }
