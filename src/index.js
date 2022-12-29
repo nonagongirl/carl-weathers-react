c81e0e54cf5c9eea71bfd2a4079733c1329a4b93
nonagongirl <amiin2deep@hotmail.com>
2022-12-29 22:13:04 +0000

    Adjusted index.js

--- src/index.js
+++ src/index.js
@@ -6,9 +6,6 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.css";
 
 import App from "./App";
-import Footer from "./Footer";
-import Search from "./Search";
-import WeatherResult from "./WeatherResult";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
@@ -16,10 +13,5 @@ const root = createRoot(rootElement);
 root.render(
   <StrictMode>
     <App />
-    <Search />
-
-    <WeatherResult />
-
-    <Footer />
   </StrictMode>
 );
