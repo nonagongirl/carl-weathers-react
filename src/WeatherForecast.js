a030583e7e8568062dec500676588fbcb437726b
nonagongirl <amiin2deep@hotmail.com>
2023-01-23 00:59:20 +0000

    Adjusted forecast to show search location and next 5 days
    Previously not uploading with city change and showing current day with next 4 days

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,23 +1,31 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import WeatherForecastDay from "./WeatherForecastDay.js";
 
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.data.city]);
+
   function handleResponse(response) {
     setForecast(response.data.daily);
     setLoaded(true);
   }
 
+  //if city changes
+  //set loaded to false
+
   if (loaded) {
     return (
       <div className="WeatherForecast appBoxes centered">
-        <h5> Your Forecast:</h5>
+        <h5> Your Forecast in Celsius:</h5>
         <div className="row">
           {forecast.map(function (dailyForecast, index) {
-            if (index < 6) {
+            if (index > 0 && index < 6) {
               return (
                 <div className="col" key={index}>
                   <WeatherForecastDay data={dailyForecast} />
